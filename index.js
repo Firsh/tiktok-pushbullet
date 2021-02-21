ebb2cd34d307f4eb4aca3fdf47bfe109df9b0af8
Firsh <firshee@gmail.com>
2021-02-21 12:25:06 +0000

    made index.js use jsdoc

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const axios = require('axios');
 const tts = require('tiktok-scraper');
 const fs = require('fs');
 require('dotenv').config();
-
+/**
+ * Get the latest pushes from Pushbullet, after a certain time.
+ * @param {*} modifiedAfter A timestamp of the most recent push, or 0 if it's the first run.
+ * @returns {Object} axios response
+ */
 async function getPushes(modifiedAfter) {
     return await axios
         .get('https://api.pushbullet.com/v2/pushes', {
@@ -18,7 +22,11 @@ async function getPushes(modifiedAfter) {
             console.log(e);
         });
 }
-
+/**
+ * Get the "real" TikTok URL that expands upon the short URL, by following the redirect.
+ * @param {*} URL A short ("share-style") TikTok URL, such as https://vm.tiktok.com/whatever/
+ * @returns {Object} axios response
+ */
 async function getFromTikTok(URL) {
     return axios
         .get(URL, {
@@ -37,6 +45,11 @@ async function getFromTikTok(URL) {
             return {success: false};
         });
 }
+/**
+ * Create a list of full TikTok video URLs, if any are found in the current batch of pushes.
+ * @param {Object} pushes The pushes object from the PushBullet API response.
+ * @returns {Array} List of TikTok web video URLs found in the current batch.
+ */
 async function newVideoLinks(pushes) {
     const shortVideoURLs = [];
     pushes.forEach((push) => {
@@ -60,21 +73,22 @@ async function newVideoLinks(pushes) {
     }
     return webVideoURLs;
 }
-
+/**
+ * Puts the list of videos into the JSON data-store.
+ * @param {Object} store The data-store.
+ * @param {Array} links List of TikTok web video URLs.
+ * @returns {Object} Maybe modified data-store.
+ */
 function storeLinks(store, links) {
     links.forEach((link) => {
         store.union('videoLinks', link);
     });
     return store;
 }
-
-async function downloadVids() {
-    return await tts.fromfile('list.txt', {
-        download: true,
-        hdVideo: true,
-        filepath: process.cwd() + '/downloads/',
-    });
-}
+/**
+ * Creates a simple txt file that is a list of videos, for tiktok-scraper.
+ * @param {Array} links Links to videos to download.
+ */
 function createList(links) {
     const joined = links.join('\r\n');
     fs.writeFile('list.txt', joined, function (err) {
@@ -82,6 +96,30 @@ function createList(links) {
         console.log(`Download list created for ${links.length} videos.`);
     });
 }
+/**
+ * Empties the txt file that is used by tiktok-scraper.
+ */
+function purgeList() {
+    fs.writeFile('list.txt', '', function (err) {
+        if (err) return console.log(err);
+    });
+}
+/**
+ * Calls tiktok-scraper's fromfile mode, and gives it the txt list.
+ * @returns {Object} A table containing the successfully downloaded videos.
+ */
+async function downloadVids() {
+    return await tts.fromfile('list.txt', {
+        download: true,
+        hdVideo: true,
+        filepath: process.cwd() + '/downloads/',
+    });
+}
+/**
+ * Matches downloaded video URLs to those in the JSON data-store, and removes those that were successfully downloaded.
+ * @param {Object} downloaded The object tiktok-scraper created after the downloading, with a table of downloaded videos.
+ * @param {Object} store The data-store.
+ */
 function purgeDownloadedFromStore(downloaded, store) {
     const videoLinks = store.get('videoLinks');
     downloaded.table.forEach((video) => {
@@ -92,22 +130,29 @@ function purgeDownloadedFromStore(downloaded, store) {
     });
     store.set('videoLinks', videoLinks);
 }
+/**
+ * Main function to control the script.
+ */
 async function init() {
     const store = require('data-store')({path: process.cwd() + '/data.json'});
     let lastModified = store.get('lastModified') | 0;
+    //  Start by getting the pushes
     const response = await getPushes(lastModified);
     const pushes = response.data.pushes;
     lastModified = pushes.length !== 0 ? pushes[0].modified : lastModified;
     store.set('lastModified', lastModified);
+    // Find the new videos (it DOES need the await)
     const newLinks = await newVideoLinks(pushes);
     if (newLinks.length > 0) {
         storeLinks(store, newLinks);
     }
+    // Make use of all existing video links to download
     const videoLinks = store.get('videoLinks');
     if (videoLinks.length > 0) {
         createList(videoLinks);
         const downloaded = await downloadVids();
         purgeDownloadedFromStore(downloaded, store);
+        purgeList();
     }
 }
 init();
